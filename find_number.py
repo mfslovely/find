9e7514d0d3fa388200301df8cb256c619f4c5bce
Lovely Gupta <lovelygupta016@gmail.com>
2024-07-23 17:30:47 +0530

    finding a number

---
+++ find_number.py
@@ -0,0 +1,9 @@
+import random
+
+user = int(input("guess a no. between 1 to 10: "))
+num = random.randint(1,10)
+
+if num == user:
+    print(f"congratulation you win {num}")
+else:
+    print(f"you failed no. is {num}")
